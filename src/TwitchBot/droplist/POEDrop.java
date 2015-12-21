789d451ce756365465ca85aba6a36ac372ebb454
Tobias Bordenca <tobiasuzt@gmail.com>
2015-12-21 12:49:42 +0100

    poe droplist lowercase fix

--- src/TwitchBot/droplist/POEDrop.java
+++ src/TwitchBot/droplist/POEDrop.java
@@ -7,7 +7,7 @@ public class POEDrop extends Drop {
 
     public POEDrop(String name) {
         this.name = toTitleCase(name);
-        this.name = this.name.replace("And", "and").replace("Of", "of").replace("At", "at").replace("To", "to").replace("The", "the");
+        this.name = this.name.replace(" And ", " and ").replace(" Of ", " of ").replace(" At ", " at ").replace(" To ", " to ").replace(" The ", " the ");
         try {
             wikiUrl = "http://pathofexile.gamepedia.com/" + URLEncoder.encode(this.name.replace(" ", "_"), "UTF-8");
         } catch (UnsupportedEncodingException e) {
